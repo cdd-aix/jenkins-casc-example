fb980e1d4a46531fc45602e1c22084e7294990b5
Chris Dukes <chris.dukes.aix@gmail.com>
2019-11-02 12:07:20 -0400

    Add basic swarm container that connects to Jenkins
    User and password not yet set

---
+++ swarm-from-docker/jenkins-swarm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eux
+# Mooched from https://github.com/carlossg/jenkins-swarm-slave-docker/blob/master/jenkins-slave.sh
+if [ "$#" -gt 0 ] && [ "$1" == "${1#-}" ]; then
+    CMD=()
+else
+    CMD=(
+	java
+	${JAVA_OPTS:+$JAVA_OPTS}
+	-jar /swarm-client-$JENKINS_SWARM_VERSION.jar
+	${MASTER:+-master "$MASTER"}
+	${LABELS:+-labels "$LABELS"}
+    )
+fi
+CMD+=("$@")
+exec "${CMD[@]}"
